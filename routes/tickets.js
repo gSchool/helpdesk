17680d7f6045211a779063685fc7ea5110eabefe
Sean Brage <seanmbrage@me.com>
2016-01-18 14:51:21 -0700

    closed tickets link now correctly displays closed tickets. had to fix the query to show tickets where is_closed was false OR null

--- routes/tickets.js
+++ routes/tickets.js
@@ -17,7 +17,7 @@ router.get('/tickets', function(req, res, next) {
 });
 
 router.get('/tickets/closed', function(req, res, next) {
-  Tickets().where({"tickets":"closed"}).then(function (tickets) {
+  Tickets().where('is_open', 'FALSE').orWhere('is_open', null).then(function (tickets) {
     res.render('tickets/closed', {tickets: tickets});
   })
 });
